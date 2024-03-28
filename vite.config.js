c88164f397e830ef069f749c527cede543de7fff
Manju Gurung <manju.gurung0912@gmail.com>
2024-03-28 12:09:48 +0530

    Firebase hosting setup and Deployement

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.m4v"],
+  build: {
+    outDir: "public"
+  },
+  assetsInclude: ['**/*.m4v']
 })

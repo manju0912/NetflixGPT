8ba1a40bf4c74385ef773e01ca4e07b89c26160e
Manju Gurung <manju.gurung0912@gmail.com>
2024-04-16 19:31:15 +0530

    Added extra info to Video card

--- src/pages/HomePage/VideoCard.jsx
+++ src/pages/HomePage/VideoCard.jsx
@@ -1,12 +1,16 @@
 import {IMG_CDN_URL} from '../../utils/constants';
 
-const VideoCard = ({posterPath}) => {
+const VideoCard = ({movieTitle, releaseDate, rating, posterPath}) => {
   if(!posterPath) return;
   
   return (
-    <div className="w-[150px] h-[200px] rounded-md shadow-lg shadow-slate-500 overflow-hidden">
-      <div className="w-full h-full rounded-md">
+    <div className="w-[160px] h-[280px] rounded-md overflow-hidden">
+      <div className="w-full h-3/4 rounded-md">
         <img src={IMG_CDN_URL + posterPath} alt="poster" className="w-full h-full object-cover" />
+      </div>
+      <div className='px-1 py-2'>
+        <h5 className='truncate'>{movieTitle}</h5>
+        <p className='text-sm font-light text-gray-500 flex justify-between'><span>{releaseDate}</span> <span>{rating}</span></p>
       </div>
     </div>
   )
